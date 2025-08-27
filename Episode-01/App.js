3e504c70d5dd8252a0c71f47cb62c8c56bb87129
Aryan0131-18 <eraryanraj18@gmail.com>
2025-08-27 19:36:34 +0530

    Added episode-01

---
+++ Episode-01/App.js
@@ -0,0 +1,9 @@
+const heading= React.createElement(
+    "h1",
+    {id:"heading"},
+    "Hello World From React!"
+);
+
+const root=ReactDOM.creatRoot(document.getElementById("root"));
+
+root.render(heading);
